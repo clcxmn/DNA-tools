26965fc57f9647e096663c231c1a9cb1e1b5ea33
clcxmn <63168144+clcxmn@users.noreply.github.com>
2020-04-10 12:32:13 -0700

    Add files via upload

---
+++ scaffold_survey.py
@@ -0,0 +1,32 @@
+"""
+Function returns sub-sequences of a chosen length (le) within intervals of a
+chosen length (itvl) across an entire sequence/scaffold (scaf) organized in
+a dictonary format in order from the beginning to the end of the scaffold
+
+saves dictionary into desired directory as seperate file which must be
+accessed using eval function
+"""
+
+#replace ls_orchid with sequence of choice, file goes in same folder as program
+with open('cleanseq','r') as file:
+    data=file.read()
+
+def scaf_survey(scaf,le,itvl):
+    b={}
+    for i in range(0,len(scaf)-1):
+        if i%itvl==0:
+            c=[]
+            for j in range(i,i+le):
+                c.append(scaf[j])
+                b[int(i/itvl)]=c
+    d=[]
+    for i in b:
+        b[i].append('\n\n\n')
+        d.append(''.join(b[i]))
+    final=''.join(d)
+    with open('C:/Users/user/Documents/post_survey','w') as data:
+        data.write(str(final))
+
+
+scaf_survey(data,1000,10000)
+
