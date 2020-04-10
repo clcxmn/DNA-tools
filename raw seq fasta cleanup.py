9414764d0c47e16e79bcb6898a314e900ef6f4db
clcxmn <63168144+clcxmn@users.noreply.github.com>
2020-04-10 12:30:52 -0700

    Add files via upload

---
+++ raw seq fasta cleanup.py
@@ -0,0 +1,28 @@
+with open('anno.txt','r') as file:
+        data=file.read()
+
+"""
+remove everything from FASTA except, A,T,C, and G, and indents (\n) since
+the file becomes practically unreadable if the whole genome is in one line
+"""
+
+li=list(data)
+b=[]
+for val in li:
+        if val=='T':
+                b.append(val)
+        elif val=='A':
+                b.append(val)
+        elif val=='C':
+                b.append(val)
+        elif val=='G':
+                b.append(val)
+        elif val=='\n':
+                b.append(val)
+        else:
+                pass
+
+c=''.join(b)
+
+with open('C:/Users/user/Documents/cleanseq','w') as data:
+        data.write(str(c))
